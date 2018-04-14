a68c6d4248e10f31a55f1da15ca294ca7620d777
Suzy (Sujung) Lee <sujunglee2020@u.northwestern.edu>
2018-04-14 17:54:59 -0500

    Implement basic header with semantic ui

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Container, Grid, Header, Icon, Button } from "semantic-ui-react";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <br />
+        <Container textAlign="center">
+          <Header as="h2" icon>
+            <Icon name="child" />
+            Chem Fam
+            <Header.Subheader>
+              Help the elements find their family!
+            </Header.Subheader>
+          </Header>
+        </Container>
       </div>
     );
   }
---
+++ src/components/FamilyLists.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+// import logo from "./logo.svg";
+// import "./App.css";
+
+class FamilyLists extends Component {
+  render() {
+    return (
+      <div>
+        <p>Hello</p>
+      </div>
+    );
+  }
+}
+
+export default FamilyLists;
