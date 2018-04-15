738071a9fc754928187f3aef4ddb166e375457db
Suzy (Sujung) Lee <sujunglee2020@u.northwestern.edu>
2018-04-14 22:14:56 -0500

    Update content for sodium

--- src/api/quizQuestions.js
+++ src/api/quizQuestions.js
@@ -25,30 +25,34 @@ var quizQuestions = [
         content: "Noble gases"
       }
     ]
+  },
+  {
+    name: "Sodium",
+    location: "Found hugging a salt shaker in 1807",
+    question:
+      "When I was in kindergarten, my bully threw me into a body of water for having a stupid name (Sodium). I exploded into flames in his face. Don't mess with me. Chlorine is my best friend, though. We like to bond and become table salt so that nobody is stuck with bland food (NaCl).",
+    image: "https://www.dropbox.com/s/w9z9x0y03pk7dfa/sodium.png?raw=1",
+    hint:
+      "There is a family of metals known to be extremely reactive. Cool chemistry teachers like to throw samples of these metals into lakes to show students how explosive they can be.",
+    answers: [
+      {
+        type: "true",
+        content: "Alkali metals"
+      },
+      {
+        type: "false1",
+        content: "Transition metals"
+      },
+      {
+        type: "false2",
+        content: "Halogens"
+      },
+      {
+        type: "false3",
+        content: "Noble gases"
+      }
+    ]
   }
-  // {
-  //   question:
-  //     "When I was in kindergarten, my bully threw me into a body of water for having a stupid name (Sodium). I exploded into flames in his face. Don't mess with me. Chlorine is my best friend, though. We like to bond and become table salt so that nobody is stuck with bland food (NaCl).",
-  //   image: "https://www.dropbox.com/s/w9z9x0y03pk7dfa/sodium.png?raw=1",
-  //   answers: [
-  //     {
-  //       type: "true",
-  //       content: "Alkali metals"
-  //     },
-  //     {
-  //       type: "false1",
-  //       content: "Transition metals"
-  //     },
-  //     {
-  //       type: "false2",
-  //       content: "Halogens"
-  //     },
-  //     {
-  //       type: "false3",
-  //       content: "Noble gases"
-  //     }
-  //   ]
-  // }
   // {
   //   question:
   //     "I've accidentally electrocuted or burned a lot of friends who try to touch me. Sorry, it wasn't intentional, I swear.",
