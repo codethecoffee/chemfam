1f43dd061c3bb5d3803325467721cb68d617cd7b
Suzy (Sujung) Lee <sujunglee2020@u.northwestern.edu>
2018-04-14 20:21:39 -0500

    Create basic template for quiz

---
+++ src/api/quizQuestions.js
@@ -0,0 +1,92 @@
+var quizQuestions = [
+  {
+    question:
+      "Hi, I'm Argon. I'm super antisocial, so much in fact that there are only 4 compounds that I'm a part of.",
+    answers: [
+      {
+        type: "false1",
+        content: "Alkali metals"
+      },
+      {
+        type: "false2",
+        content: "Transition metals"
+      },
+      {
+        type: "false3",
+        content: "Halogens"
+      },
+      {
+        type: "true",
+        content: "Noble gases"
+      }
+    ]
+  },
+  {
+    question:
+      "When I was in kindergarten, my bully threw me into a body of water for having a stupid name (Sodium). I exploded into flames in his face. Don't mess with me. Chlorine is my best friend, though. We like to bond and become table salt so that nobody is stuck with bland food (NaCl).",
+    answers: [
+      {
+        type: "true",
+        content: "Alkali metals"
+      },
+      {
+        type: "false1",
+        content: "Transition metals"
+      },
+      {
+        type: "false2",
+        content: "Halogens"
+      },
+      {
+        type: "false3",
+        content: "Noble gases"
+      }
+    ]
+  },
+  {
+    question:
+      "I've accidentally electrocuted or burned a lot of friends who try to touch me. Sorry, it wasn't intentional, I swear.",
+    answers: [
+      {
+        type: "false1",
+        content: "Alkali metals"
+      },
+      {
+        type: "true",
+        content: "Transition metals"
+      },
+      {
+        type: "false3",
+        content: "Halogens"
+      },
+      {
+        type: "false4",
+        content: "Noble gases"
+      }
+    ]
+  },
+  {
+    question:
+      "Engineers really like me because they can control exactly when electricity will run through me or not. I heard that a group of them named an entire place after me: Silicon Valley. It's a bit obsessive, actually.",
+    answers: [
+      {
+        type: "false1",
+        content: "Alkali metals"
+      },
+      {
+        type: "false2",
+        content: "Transition metals"
+      },
+      {
+        type: "false3",
+        content: "Halogens"
+      },
+      {
+        type: "true",
+        content: "Metalloid"
+      }
+    ]
+  }
+];
+
+export default quizQuestions;
